b678445da79b79bd258295ec3d093099f233d70a
Drew Payment <drew@verostack.io>
2019-06-30 22:02:28 -0400

    partial

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,8 +6,6 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-        //Google Maps API Key
-        [GMSServices provideAPIKey:@"AIzaSyBOlHeDtx3CO0CLBhJhmLoKlkdlOEpDs9M"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,37 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let json = readJSONFromFile(fileName: "secrets")
+    if let jsonResult = json as? Dictionary<String, AnyObject>, let key = jsonResult["google_maps_sdk_api"] as? String {
+        GMSServices.provideAPIKey(key)
+        print(key)
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    func readJSONFromFile(fileName: String) -> Any?
+    {
+        var json: Any?
+        if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
+            do {
+                let fileUrl = URL(fileURLWithPath: path)
+                // get json file
+                let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+                json = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            } catch {
+                // catch error here...
+                print("Unable to deserialize")
+            }
+        }
+        return json
+    }
 }
